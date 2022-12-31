c55932fd4599413a152d6fb1f4c236e35dab0be6
jay <codergogoi@gmail.com>
2022-12-31 14:58:41 +0100

    Updated signature validation function

--- online_shopping_monolithic/src/config/index.js
+++ online_shopping_monolithic/src/config/index.js
@@ -1,16 +1,14 @@
-const dotEnv  = require("dotenv");
+const dotEnv = require("dotenv");
 
-if (process.env.NODE_ENV !== 'prod') {
-    const configFile =  `./.env.${process.env.NODE_ENV}`;
-    dotEnv.config({ path:  configFile });
+if (process.env.NODE_ENV !== "prod") {
+  const configFile = `./.env.${process.env.NODE_ENV}`;
+  dotEnv.config({ path: configFile });
 } else {
-    dotEnv.config();
+  dotEnv.config();
 }
 
 module.exports = {
-
-    PORT: process.env.PORT,
-    DB_URL: process.env.MONGODB_URI,
-    APP_SECRET: process.env.APP_SECRET
-}
- 
+  PORT: process.env.PORT,
+  DB_URL: process.env.MONGODB_URI,
+  APP_SECRET: process.env.APP_SECRET,
+};
--- online_shopping_monolithic/src/utils/index.js
+++ online_shopping_monolithic/src/utils/index.js
@@ -1,45 +1,51 @@
-const bcrypt = require('bcrypt');
-const jwt  = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const { APP_SECRET } = require('../config');
+const { APP_SECRET } = require("../config");
 
 //Utility functions
-module.exports.GenerateSalt = async() => {
-        return await bcrypt.genSalt()    
-},
-
-module.exports.GeneratePassword = async (password, salt) => {
-        return await bcrypt.hash(password, salt);
+module.exports.GenerateSalt = async () => {
+  return await bcrypt.genSalt();
 };
 
+module.exports.GeneratePassword = async (password, salt) => {
+  return await bcrypt.hash(password, salt);
+};
 
-module.exports.ValidatePassword = async (enteredPassword, savedPassword, salt) => {
-        return await this.GeneratePassword(enteredPassword, salt) === savedPassword;
+module.exports.ValidatePassword = async (
+  enteredPassword,
+  savedPassword,
+  salt
+) => {
+  return (await this.GeneratePassword(enteredPassword, salt)) === savedPassword;
 };
 
 module.exports.GenerateSignature = async (payload) => {
-        return await jwt.sign(payload, APP_SECRET, { expiresIn: '1d'} )
-}, 
+  try {
+    return await jwt.sign(payload, APP_SECRET, { expiresIn: "30d" });
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
 
-module.exports.ValidateSignature  = async(req) => {
-
-        const signature = req.get('Authorization');
-
-        console.log(signature);
-        
-        if(signature){
-            const payload = await jwt.verify(signature.split(' ')[1], APP_SECRET);
-            req.user = payload;
-            return true;
-        }
-
-        return false
+module.exports.ValidateSignature = async (req) => {
+  try {
+    const signature = req.get("Authorization");
+    console.log(signature);
+    const payload = await jwt.verify(signature.split(" ")[1], APP_SECRET);
+    req.user = payload;
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 };
 
 module.exports.FormateData = (data) => {
-        if(data){
-            return { data }
-        }else{
-            throw new Error('Data Not found!')
-        }
-    }
+  if (data) {
+    return { data };
+  } else {
+    throw new Error("Data Not found!");
+  }
+};
