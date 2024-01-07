35d2459dd3a73c70661f8af74265fcc4b3c6731a
Sang Kha <80106348+khasang12-khmt@users.noreply.github.com>
2024-01-07 11:55:33 +0700

    minor fix

--- online_shopping_monolithic/product/src/express-app.js
+++ online_shopping_monolithic/product/src/express-app.js
@@ -1,21 +1,18 @@
-const express = require('express');
-const cors  = require('cors');
-const { products, appEvents } = require('./api');
-const HandleErrors = require('./utils/error-handler')
-
+const express = require("express");
+const cors = require("cors");
+const { products, appEvents } = require("./api");
+const HandleErrors = require("./utils/error-handler");
 
 module.exports = async (app, channel) => {
+  app.use(express.json({ limit: "1mb" }));
+  app.use(express.urlencoded({ extended: true, limit: "1mb" }));
+  app.use(cors());
+  app.use(express.static(__dirname + "/public"));
 
-    app.use(express.json({ limit: '1mb'}));
-    app.use(express.urlencoded({ extended: true, limit: '1mb'}));
-    app.use(cors());
-    app.use(express.static(__dirname + '/public'))
+  //api
+  //appEvents(app);
+  products(app, channel);
 
-    //api
-    //appEvents(app);
-    products(app, channel);
-
-    // error handling
-    app.use(HandleErrors);
-    
-}
+  // error handling
+  app.use(HandleErrors);
+};
